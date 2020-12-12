d02cbdb2011c01cffd654ac33ef08a8044508524
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2020-12-12 14:54:06 +0100

    Star adding database

--- datadb.cpp
+++ datadb.cpp
@@ -13,19 +13,27 @@ model::DataDB::DataDB() {
         if(!db.open())
             qWarning() << "ERROR: " << db.lastError();
 
+        QSqlQuery enableForeignKey("PRAGMA foreign_keys = ON");
+        if(!enableForeignKey.isActive())
+            qWarning() << "ERROR: " << enableForeignKey.lastError().text();
+
         QSqlQuery createUser("create table if not exists `user`(name text, primary key (name))");
         if(!createUser.isActive())
             qWarning() << "ERROR: " << createUser.lastError().text();
 
-        QSqlQuery createPC("create table if not exists pc(name text, author text, primary key (name), foreign key (author) references user (name))");
+        QSqlQuery createAdmin("create table if not exists admin(name text, primary key (name))");
+        if(!createAdmin.isActive())
+            qWarning() << "ERROR: " << createAdmin.lastError().text();
+
+        QSqlQuery createPC("create table if not exists pc(name text, author text, primary key (name), foreign key (author) references user (name) on delete cascade on update cascade)");
         if(!createPC.isActive())
             qWarning() << "ERROR: " << createPC.lastError().text();
 
-        QSqlQuery createProductor("create table if not exists productor(name text, pc_name text, primary key (name), foreign key (pc_name) references pc (name))");
+        QSqlQuery createProductor("create table if not exists productor(name text, pc_name text, primary key (name), foreign key (pc_name) references pc (name) on delete cascade on update cascade)");
         if(!createProductor.isActive())
             qWarning() << "ERROR: " << createProductor.lastError().text();
 
-        QSqlQuery createProduct("create table if not exists product(name text, price integer, pc_name text, productor_name text, primary key (name), foreign key (pc_name) references pc (name) foreign key (productor_name ) references productor(name))");
+        QSqlQuery createProduct("create table if not exists product(name text, price integer, pc_name text, productor_name text, primary key (name), foreign key (pc_name) references pc (name) on delete cascade on update cascade, foreign key (productor_name) references productor(name) on delete cascade on update cascade)");
         if(!createProduct.isActive())
             qWarning() << "ERROR: " << createProduct.lastError().text();
 
@@ -36,40 +44,41 @@ model::DataDB::DataDB() {
 
 unsigned int model::DataDB::getUserSize()
 {
-
-}
-
-model::User model::DataDB::getUser(int i)
-{
+    QSqlQuery query;
+    query.prepare("select count(name) from `user` group by name");
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
+    while (query.next()) {
+         return query.value(0).toInt();
+    }
+    return -1;
 }
 
 model::User model::DataDB::getUser(QString name)
 {
     QSqlQuery query;
-    query.prepare("select name from 'user' where name like ?");
+    query.prepare("select name from `user` where name like ?");
     query.addBindValue(name);
     if(!query.exec())
         qWarning() << "ERROR: " << query.lastError().text();
     while (query.next()) {
-         QString name = query.value(0).toString();
-         QString qname = name;
-         model::User u(qname);
+         model::User u(query.value(0).toString());
          return u;
     }
     model::User err("ERROR");
     return err;
 }
 
-std::vector<model::User> model::DataDB::getUser()
+std::vector<model::User> model::DataDB::getUsers()
 {
     QSqlQuery query;
-    query.prepare("select * from 'user'");
+    query.prepare("select * from `user`");
     if(!query.exec())
         qWarning() << "ERROR: " << query.lastError().text();
 
     std::vector<model::User> users;
     while (query.next()) {
-        User u((QString)(query.value(0).toString()));
+        User u(query.value(0).toString());
         users.push_back(u);
     }
     return users;
@@ -77,22 +86,47 @@ std::vector<model::User> model::DataDB::getUser()
 
 unsigned int model::DataDB::getPCSize()
 {
-
-}
-
-model::PC model::DataDB::getPC(int i)
-{
-
+    QSqlQuery query;
+    query.prepare("select count(name) from pc group by name");
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
+    while (query.next()) {
+         return query.value(0).toInt();
+    }
+    return -1;
 }
 
 model::PC model::DataDB::getPC(QString name)
 {
-
+    QSqlQuery query;
+    query.prepare("select name, author from pc where name like ?");
+    query.addBindValue(name);
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
+    while (query.next()) {
+         model::User qauthor(query.value(1).toString());
+         model::PC pc(query.value(0).toString(), qauthor);
+         return pc;
+    }
+    model::User errU("ERROR");
+    model::PC err("ERROR", errU);
+    return err;
 }
 
-std::vector<model::PC> model::DataDB::getPC()
+std::vector<model::PC> model::DataDB::getPCs()
 {
+    QSqlQuery query;
+    query.prepare("select * from pc");
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
 
+    std::vector<model::PC> pcs;
+    while (query.next()) {
+        User u(query.value(1).toString());
+        PC pc(query.value(0).toString(), u);
+        pcs.push_back(pc);
+    }
+    return pcs;
 }
 
 void model::DataDB::addUser(User u)
@@ -110,56 +144,85 @@ void model::DataDB::addUser(User u)
 
 void model::DataDB::addPC(PC p)
 {
-
+    if(p.getName().compare("ERROR")==0)
+    {
+        return;
+    }
+    QSqlQuery query;
+    query.prepare("insert into pc values(?, ?)");
+    query.addBindValue(p.getName());
+    query.addBindValue(p.getCreatorName());
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
 }
 
 void model::DataDB::removeUser(QString name)
 {
-
+    QSqlQuery query;
+    query.prepare("delete from `user` where name=?");
+    query.addBindValue(name);
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
 }
 
 void model::DataDB::removePC(QString name)
 {
-
-}
-
-void model::DataDB::removeUser(int i)
-{
-
-}
-
-void model::DataDB::removePC(int i)
-{
-
+    QSqlQuery query;
+    query.prepare("delete from pc where name=?");
+    query.addBindValue(name);
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
 }
 
 void model::DataDB::addAdmin(model::Admin a)
 {
-
+    if(a.getName().compare("ERROR")==0)
+    {
+        return;
+    }
+    QSqlQuery query;
+    query.prepare("insert into admin values(?)");
+    query.addBindValue(a.getName());
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
 }
 
 void model::DataDB::removeAdmin(QString name)
 {
-
-}
-
-void model::DataDB::removeAdmin(int i)
-{
-
-}
-
-model::Admin model::DataDB::getAdmin(int i)
-{
-
+    QSqlQuery query;
+    query.prepare("delete from admin where name=?");
+    query.addBindValue(name);
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
 }
 
 model::Admin model::DataDB::getAdmin(QString name)
 {
-
+    QSqlQuery query;
+    query.prepare("select name from admin where name like ?");
+    query.addBindValue(name);
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
+    while (query.next()) {
+         model::Admin a(query.value(0).toString());
+         return a;
+    }
+    model::Admin err("ERROR");
+    return err;
 }
 
-std::vector<model::Admin> model::DataDB::getAdmin()
+std::vector<model::Admin> model::DataDB::getAdmins()
 {
+    QSqlQuery query;
+    query.prepare("select * from `user`");
+    if(!query.exec())
+        qWarning() << "ERROR: " << query.lastError().text();
 
+    std::vector<model::Admin> admins;
+    while (query.next()) {
+        Admin a(query.value(0).toString());
+        admins.push_back(a);
+    }
+    return admins;
 }
 
--- datadb.h
+++ datadb.h
@@ -19,13 +19,6 @@ namespace model {
         unsigned int getUserSize();
 
         /**
-         * @brief récupere l'utilisateur à l'index i
-         * @param i l'index
-         * @return l'utilisateur
-         */
-        model::User getUser(int i);
-
-        /**
          * @brief récupere l'utilisateur avec le nom name
          * @param name le nom
          * @re+turn l'utilisateur
@@ -36,20 +29,13 @@ namespace model {
          * @brief récupere une liste des utilisateurs
          * @return une liste des utilisateurs
          */
-        std::vector<model::User> getUser();
+        std::vector<model::User> getUsers();
 
         /**
          * @brief récupere le nombre de point de collecte
          * @return le nombre de point de collecte
          */
         unsigned int getPCSize();
-
-        /**
-         * @brief récupere le point de collecte à l'index i
-         * @param i l'index
-         * @return le point de collecte
-         */
-        model::PC getPC(int i);
 
         /**
          * @brief récupere le point de collecte avec le nom name
@@ -62,7 +48,7 @@ namespace model {
          * @brief récupere une liste des points de collecte
          * @return une liste des points de collecte
          */
-        std::vector<model::PC> getPC();
+        std::vector<model::PC> getPCs();
 
         /**
          * @brief ajoute un utilisateur
@@ -89,18 +75,6 @@ namespace model {
         void removePC(QString name);
 
         /**
-         * @brief retire l'utilisateur a l'index i
-         * @param i l'index
-         */
-        void removeUser(int i);
-
-        /**
-         * @brief retire le point de collecte avec le nom name
-         * @param i l'index
-         */
-        void removePC(int i);
-
-        /**
          * @brief ajoute un administrateur
          * @param a l'administrateur à ajouter
          */
@@ -113,19 +87,6 @@ namespace model {
         void removeAdmin(QString name);
 
         /**
-         * @brief supprime un administrateur
-         * @param i l'index
-         */
-        void removeAdmin(int i);
-
-        /**
-         * @brief récupere un administrateur
-         * @param i l'index
-         * @return l'administrateur
-         */
-        model::Admin getAdmin(int i);
-
-        /**
          * @brief récupere un administrateur
          * @param name le nom de l'administrateur
          * @return l'administrateur
@@ -136,7 +97,7 @@ namespace model {
          * @brief retourne la liste des administrateurs
          * @return la liste des administrateurs
          */
-        std::vector<model::Admin> getAdmin();
+        std::vector<model::Admin> getAdmins();
     };
 }
 #endif // DATADB_H
